d948d16d53b7f6331f8f5e7c1ad23a662e177cb6
Juanmi González <juanmi.gonzalezm@icloud.com>
2026-02-14 10:49:20 +0000

    feat: integrate Firebase configuration and hosting setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,14 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideTranslateService } from '@ngx-translate/core';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -24,5 +28,8 @@ export const appConfig: ApplicationConfig = {
       fallbackLang: 'es',
       lang: 'es',
     }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'i-dont-wanna-do-it',
+    appId: '1:409866127060:web:17cc7913c94e6064009d41',
+    storageBucket: 'i-dont-wanna-do-it.firebasestorage.app',
+    apiKey: 'AIzaSyA2WxLoUvKFTt3ezUdZqQSPF1pOu_NQDAs',
+    authDomain: 'i-dont-wanna-do-it.firebaseapp.com',
+    messagingSenderId: '409866127060',
+  },
+};
